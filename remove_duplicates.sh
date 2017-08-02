5f03437a263e3790f84658b697d466e5eefeed6f
talk2shashank09 <talk2shashank09@gmail.com>
2017-08-02 10:17:47 +0530

    Update remove_duplicates.sh

--- remove_duplicates.sh
+++ remove_duplicates.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
-
-# We assume that packages can ne insall on our server, Installing rdfind package.
-# rdfind finds duplicate files in a directory and perform specified operation on them.
-# for our need, here we are repalcing duplicate files with hardlinks
-# By default rdfind search in a directory in alphabetical order.
-# Taking input as argument from user.
-
-if [ $# -eq 1 ]
-
+dir=$1
+echo "finding duplicates in the directory: $dir"
+#creating a file with checksum details
+find $dir -maxdepth 1 -type f -exec md5sum {} \; > checksum_file
+#finding the number of duplicates in the given directory using checksum not recursively in subdirectories
+cat checksum_file | awk -F" " '{print $1}'|sort|uniq -d > duplicates_file
+number_of_duplicates=`cat duplicates_file |wc -l`
+echo "number of dulicates in the directory: $number_of_duplicates"
+cat duplicates_file | while read line
+do
+    #creating a file with the names of files with the duplicate data
+    cat checksum_file | grep $line| sort|awk -F" " '{print $2}'> files_names_file
+    echo "duplicate files slot : "
+    cat files_names_file
+    #calculating the count od duplicate files
+    file_no=`cat files_names_file | wc -l`
+    #getting the name of the file which is first in the alphebatically order
+    src_file=`cat files_names_file | head -1`
+    file_no=$(($file_no - 1))
+    temp=$file_no
+    while [ $file_no -gt 0 ]
+    do
+        #getting the name of the file which is the duplicate of the source file
+        file_name=`cat files_names_file|tail -$file_no|head -1`
+        #removing the duplicate file
+        rm $file_name
+        echo "removed the file $file_name"
+        if [ $file_no -eq $temp ]
+        then
+            #creating hardlink of the duplicate file
+            ln $src_file $file_name
+            echo "created the hard link for the file $file_name"
+        fi
+        file_no=$(($file_no - 1))
+    done
+done
+#removing temperary files generated in the run time
+if [ -f checksum_file ] && [ -f duplicates_file ] && [ -f files_names_file ]
 then
-  if [ ! -x $1 ]
-    then
-      echo " This directory doesnt have executable permission "
-  else
-      rdfind -makehardlinks true $1 > /dev/null
-
-# The output of rdfind command is redirecting to black box to prevent any STDOUT.
-# above Command will repalce all existing duplicate files and create hardlinks
-
-echo " Your duplicate files has been replaced by hardlinks. you can check the result in auto generated file result.txt in the directory from where you run the script."
-
-fi
-else
- echo " Please specifiy only one directory path as argument"
+   rm checksum_file duplicates_file files_names_file
 fi
